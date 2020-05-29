b03e6c1b96cc820240e355fd46448e5b85856b8d
Matthew D. Hall <zijistark@gmail.com>
2020-05-29 07:24:06 -0700

    initial commit; map time is scaled 2x

---
+++ CampaignTimePatches.cs
@@ -0,0 +1,428 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using TaleWorlds.CampaignSystem;
+
+namespace QuickTime
+{
+    [HarmonyPatch(typeof(CampaignTime))]
+	public class CampaignTimePatches
+	{
+		/* fundamental campaign time rate multiplier */
+		private const long   TimeMultL = 2L; // integer numerator of time factor
+		private const long   TimeDivL  = 1L; // integer denominator of time factor
+		private const double TimeMult  = (double)TimeMultL / (double)TimeDivL;
+		private const float  TimeMultF = (float)TimeMult;
+
+		/* units (only one adjusted from vanilla -- days per week) */
+		private const long MsecPerSecL    = 1000L;
+		private const long SecPerMinL     = 60L;
+		private const long MinPerHourL    = 60L;
+		private const long HourPerDayL    = 24L;
+		private const long DayPerWeekL    = 3L;
+		private const long WeekPerSeasonL = 3L;
+		private const long SeasonPerYearL = 4L;
+
+		/* ticks per unit */
+		private const long TickPerMsecL   = 10L;
+		private const long TickPerSecL    = TickPerMsecL * MsecPerSecL;
+		private const long TickPerMinL    = TickPerSecL * SecPerMinL;
+		private const long TickPerHourL   = TickPerMinL * MinPerHourL;
+		private const long TickPerDayL    = TickPerHourL * HourPerDayL;
+		private const long TickPerWeekL   = TickPerDayL * DayPerWeekL;
+		private const long TickPerSeasonL = TickPerWeekL * WeekPerSeasonL;
+		private const long TickPerYearL   = TickPerSeasonL * SeasonPerYearL;
+
+		/* necessary reflection preliminaries */
+		private static readonly FieldInfo TicksFI = AccessTools.Field(typeof(CampaignTime), "_numTicks");
+		private static readonly ConstructorInfo CtorCI = AccessTools.Constructor(typeof(CampaignTime), new[] { typeof(long) });
+		private static readonly MethodInfo CurrentTicksMI = AccessTools.PropertyGetter(typeof(CampaignTime), "CurrentTicks");
+
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		/* Elapsed[UNIT]sUntilNow */
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ElapsedMillisecondsUntilNow", MethodType.Getter)]
+		static void ElapsedMillisecondsUntilNow(ref float __result)
+		{
+			__result *= TimeMultF;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ElapsedSecondsUntilNow", MethodType.Getter)]
+		static void ElapsedSecondsUntilNow(ref float __result)
+		{
+			__result *= TimeMultF;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ElapsedHoursUntilNow", MethodType.Getter)]
+		static void ElapsedHoursUntilNow(ref float __result)
+		{
+			__result *= TimeMultF;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ElapsedDaysUntilNow", MethodType.Getter)]
+		static void ElapsedDaysUntilNow(ref float __result)
+		{
+			__result *= TimeMultF;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ElapsedWeeksUntilNow", MethodType.Getter)]
+		static void ElapsedWeeksUntilNow(ref float __result)
+		{
+			__result *= TimeMultF;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ElapsedSeasonsUntilNow", MethodType.Getter)]
+		static void ElapsedSeasonsUntilNow(ref float __result)
+		{
+			__result *= TimeMultF;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ElapsedYearsUntilNow", MethodType.Getter)]
+		static void ElapsedYearsUntilNow(ref float __result)
+		{
+			__result *= TimeMultF;
+		}
+
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		/* Remaining[UNIT]sFromNow */
+
+		[HarmonyPostfix]
+		[HarmonyPatch("RemainingMillisecondsFromNow", MethodType.Getter)]
+		static void RemainingMillisecondsFromNow(ref float __result)
+		{
+			__result *= TimeMultF;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("RemainingSecondsFromNow", MethodType.Getter)]
+		static void RemainingSecondsFromNow(ref float __result)
+		{
+			__result *= TimeMultF;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("RemainingHoursFromNow", MethodType.Getter)]
+		static void RemainingHoursFromNow(ref float __result)
+		{
+			__result *= TimeMultF;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("RemainingDaysFromNow", MethodType.Getter)]
+		static void RemainingDaysFromNow(ref float __result)
+		{
+			__result *= TimeMultF;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("RemainingWeeksFromNow", MethodType.Getter)]
+		static void RemainingWeeksFromNow(ref float __result)
+		{
+			__result *= TimeMultF;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("RemainingSeasonsFromNow", MethodType.Getter)]
+		static void RemainingSeasonsFromNow(ref float __result)
+		{
+			__result *= TimeMultF;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("RemainingYearsFromNow", MethodType.Getter)]
+		static void RemainingYearsFromNow(ref float __result)
+		{
+			__result *= TimeMultF;
+		}
+
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		/* To[UNIT]s */
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ToMilliseconds", MethodType.Getter)]
+		static void ToMilliseconds(ref double __result)
+		{
+			__result *= TimeMult;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ToSeconds", MethodType.Getter)]
+		static void ToSeconds(ref double __result)
+		{
+			__result *= TimeMult;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ToMinutes", MethodType.Getter)]
+		static void ToMinutes(ref double __result)
+		{
+			__result *= TimeMult;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ToHours", MethodType.Getter)]
+		static void ToHours(ref double __result)
+		{
+			__result *= TimeMult;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ToDays", MethodType.Getter)]
+		static void ToDays(ref double __result)
+		{
+			__result *= TimeMult;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ToWeeks", MethodType.Getter)]
+		static void ToWeeks(ref double __result)
+		{
+			__result *= TimeMult;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ToSeasons", MethodType.Getter)]
+		static void ToSeasons(ref double __result)
+		{
+			__result *= TimeMult;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch("ToYears", MethodType.Getter)]
+		static void ToYears(ref double __result)
+		{
+			__result *= TimeMult;
+		}
+
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		/* Get[UNIT]Of[UNIT] */
+
+		[HarmonyPrefix]
+		[HarmonyPatch("GetHourOfDay", MethodType.Getter)]
+		static bool GetHourOfDay(ref CampaignTime __instance, ref int __result)
+		{
+			__result = (int)((long)TicksFI.GetValue(__instance) * TimeMultL / TimeDivL / 36000000L % 24L);
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("GetDayOfWeek", MethodType.Getter)]
+		static bool GetDayOfWeek(ref CampaignTime __instance, ref int __result)
+		{
+			__result = (int)((long)TicksFI.GetValue(__instance) * TimeMultL / TimeDivL / 864000000L % 7L);
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("GetDayOfSeason", MethodType.Getter)]
+		static bool GetDayOfSeason(ref CampaignTime __instance, ref int __result)
+		{
+			__result = (int)((long)TicksFI.GetValue(__instance) * TimeMultL / TimeDivL / 864000000L % 21L);
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("GetDayOfYear", MethodType.Getter)]
+		static bool GetDayOfYear(ref CampaignTime __instance, ref int __result)
+		{
+			__result = (int)((long)TicksFI.GetValue(__instance) * TimeMultL / TimeDivL / 864000000L % 84L);
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("GetWeekOfSeason", MethodType.Getter)]
+		static bool GetWeekOfSeason(ref CampaignTime __instance, ref int __result)
+		{
+			__result = (int)((long)TicksFI.GetValue(__instance) * TimeMultL / TimeDivL / 6048000000L % 3L);
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("GetSeasonOfYear", MethodType.Getter)]
+		static bool GetSeasonOfYear(ref CampaignTime __instance, ref int __result)
+		{
+			__result = (int)((long)TicksFI.GetValue(__instance) * TimeMultL / TimeDivL / 18144000000L % 4L);
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("GetYear", MethodType.Getter)]
+		static bool GetYear(ref CampaignTime __instance, ref int __result)
+		{
+			__result = (int)((long)TicksFI.GetValue(__instance) * TimeMultL / TimeDivL / 72576000000L);
+			return false;
+		}
+
+		/////////////////////////////////////////////////////////////////////////////////////////////
+
+		[HarmonyPrefix]
+		[HarmonyPatch("GetDayOfSeasonf", MethodType.Getter)]
+		static bool GetDayOfSeasonf(ref CampaignTime __instance, ref float __result)
+		{
+			__result = (float)Math.IEEERemainder((double)((long)TicksFI.GetValue(__instance) / 864000000L), 21.0);
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("GetSeasonOfYearf", MethodType.Getter)]
+		static bool GetSeasonOfYearf(ref CampaignTime __instance, ref float __result)
+		{
+			__result = (float)Math.IEEERemainder((double)((long)TicksFI.GetValue(__instance) / 18144000000L), 4.0);
+			return false;
+		}
+
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		/* [UNIT]s (factory methods) */
+
+		[HarmonyPrefix]
+		[HarmonyPatch("Milliseconds")]
+		static bool Milliseconds(long valueInMilliseconds, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[] { valueInMilliseconds * TimeDivL / TimeMultL * 10L });
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("Seconds")]
+		static bool Seconds(long valueInSeconds, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[] { valueInSeconds * TimeDivL / TimeMultL * 10000L });
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("Minutes")]
+		static bool Minutes(long valueInMinutes, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[] { valueInMinutes * TimeDivL / TimeMultL * 600000L });
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("Hours")]
+		static bool Hours(float valueInHours, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[] { (long)(valueInHours / TimeMultF * 3.6E+07f) });
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("Days")]
+		static bool Days(float valueInDays, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[] { (long)(valueInDays / TimeMultF * 8.64E+08f) });
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("Weeks")]
+		static bool Weeks(float valueInWeeeks, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[] { (long)(valueInWeeeks / TimeMultF * 6.048E+09f) });
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("Seasons")]
+		static bool Seasons(float valueInSeasons, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[] { (long)(valueInSeasons / TimeMultF * 1.8144E+10f) });
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("Years")]
+		static bool Years(float valueInYears, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[] { (long)(valueInYears / TimeMultF * 7.2576E+10f) });
+			return false;
+		}
+
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		/* [UNIT]sFromNow (factory methods) */
+
+		[HarmonyPrefix]
+		[HarmonyPatch("MillisecondsFromNow")]
+		static bool MillisecondsFromNow(long valueInMilliseconds, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[]
+			{
+				(long)CurrentTicksMI.Invoke(null, null) + valueInMilliseconds * TimeDivL / TimeMultL * 10L
+			});
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("SecondsFromNow")]
+		static bool SecondsFromNow(long valueInSeconds, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[]
+			{
+				(long)CurrentTicksMI.Invoke(null, null) + valueInSeconds * TimeDivL / TimeMultL * 10000L
+			});
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("MinutesFromNow")]
+		static bool MinutesFromNow(long valueInMinutes, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[]
+			{
+				(long)CurrentTicksMI.Invoke(null, null) + valueInMinutes * TimeDivL / TimeMultL * 600000L
+			});
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("HoursFromNow")]
+		static bool HoursFromNow(float valueInHours, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[]
+			{
+				(long)CurrentTicksMI.Invoke(null, null) + (long)(valueInHours / TimeMultF * 3.6E+07f)
+			});
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("DaysFromNow")]
+		static bool DaysFromNow(float valueInDays, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[]
+			{
+				(long)CurrentTicksMI.Invoke(null, null) + (long)(valueInDays / TimeMultF * 8.64E+08f)
+			});
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("WeeksFromNow")]
+		static bool WeeksFromNow(float valueInWeeks, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[]
+			{
+				(long)CurrentTicksMI.Invoke(null, null) + (long)(valueInWeeks / TimeMultF * 6.048E+09f)
+			});
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch("YearsFromNow")]
+		static bool YearsFromNow(float valueInYears, ref CampaignTime __result)
+		{
+			__result = (CampaignTime)CtorCI.Invoke(new object[]
+			{
+				(long)CurrentTicksMI.Invoke(null, null) + (long)(valueInYears / TimeMultF * 7.2576E+10f)
+			});
+			return false;
+		}
+	}
+}
---
+++ SubModule.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+
+namespace QuickTime
+{
+	public class SubModule : MBSubModuleBase
+	{
+		public static readonly string ID = typeof(SubModule).Namespace;
+		public static readonly string Name = "Swift Time";
+		public static readonly string Version = "v0.1.0";
+		public static readonly string HarmonyID = "com.zijistark.bannerlord." + ID.ToLower();
+
+		protected override void OnSubModuleLoad()
+		{
+			base.OnSubModuleLoad();
+			var harmony = new Harmony(HarmonyID);
+			harmony.PatchAll();
+		}
+
+		protected override void OnBeforeInitialModuleScreenSetAsRoot()
+		{
+			if (!_loaded)
+			{
+				InformationManager.DisplayMessage(new InformationMessage($"~ Loaded {Name} {Version}", Color.FromUint(0x00007CD7)));
+				_loaded = true;
+			}
+		}
+
+		private bool _loaded = false;
+	}
+}
