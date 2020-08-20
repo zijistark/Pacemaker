bf5de9a6e28bae28f8dfcc1ca008d2a832cf0784
Matthew D. Hall <zijistark@gmail.com>
2020-08-19 21:46:21 -0700

    misc

--- src/Patches/MapTimeTrackerTickPatch.cs
+++ src/Patches/MapTimeTrackerTickPatch.cs
@@ -11,14 +11,8 @@ namespace Pacemaker.Patches
 		private static readonly Type MapTimeTrackerT = AccessTools.PropertyGetter(typeof(Campaign), "MapTimeTracker").ReturnType;
 		private static readonly MethodInfo TargetMI = AccessTools.Method(MapTimeTrackerT, "Tick");
 
-		static MethodBase TargetMethod()
-		{
-			return TargetMI;
-		}
+		static MethodBase TargetMethod() => TargetMI;
 
-		static void Prefix(ref float seconds)
-		{
-			seconds *= Main.Settings.TimeMultiplier;
-		}
+		static void Prefix(ref float seconds) => seconds *= Main.Settings.TimeMultiplier;
 	}
 }
