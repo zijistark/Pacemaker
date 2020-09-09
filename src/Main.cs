5da75c916a8e71ad8cb456325dbb0cd0037ffc1b
Matthew D. Hall <zijistark@gmail.com>
2020-09-09 07:09:06 -0700

    version up

--- src/Main.cs
+++ src/Main.cs
@@ -13,8 +13,8 @@ namespace Pacemaker
 		/* Semantic Versioning (https://semver.org): */
 		public const int SemVerMajor = 1;
 		public const int SemVerMinor = 1;
-		public const int SemVerPatch = 0;
-		public const string SemVerSpecial = "beta1";
+		public const int SemVerPatch = 1;
+		public const string SemVerSpecial = null;
 		private static readonly string SemVerEnd = (SemVerSpecial != null) ? '-' + SemVerSpecial : string.Empty;
 		public static readonly string Version = $"{SemVerMajor}.{SemVerMinor}.{SemVerPatch}{SemVerEnd}";
 
